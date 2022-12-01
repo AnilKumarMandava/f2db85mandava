d723d7de2caf2d5db3afadd738e4c9b6995b716a
AnilKumarMandava <mailtoanilmandava@gmail.com>
2022-12-01 00:30:12 -0600

    bonus

--- models/event.js
+++ models/event.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const EventSchema = mongoose.Schema({
-Eventname: String,
-Event_id: Number,
-Eventtype: String
+Eventname:{ type: String, minlength: '1', maxlength:'20'},
+Event_id:{ type: Number, min:'1', max:'2500' },
+Eventtype:{type: String, minlength: '1', maxlength:'20'}
 })
 module.exports = mongoose.model("Event",
 EventSchema)
