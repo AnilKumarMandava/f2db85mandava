354eada8551d21af048947b6f23374da3cf0ccbf
AnilKumarMandava <mailtoanilmandava@gmail.com>
2022-11-30 20:03:48 -0600

    Bonus

--- routes/event.js
+++ routes/event.js
@@ -5,18 +5,33 @@ var router = express.Router();
 router.get('/', Event_controlers.Event_view_all_Page);
 /* GET detail costume page */
 router.get('/detail', Event_controlers.Event_view_one_Page);
-/* GET create costume page */
-router.get('/create', Event_controlers.Event_create_Page);
-/* GET create update page */
-router.get('/update', Event_controlers.Event_update_Page);
-/* GET delete costume page */
-router.get('/delete', Event_controlers.Event_delete_Page);
-router.get('/:id', Event_controlers.Event_detail);
-router.put('/:id',Event_controlers.Event_update_put);
+
 /* GET home page. */
 /*router.get('/', Event_controlers.watch_view_all_Page);*/
 /* GET detail watch page */
 /*router.get('/detail', watch_controlers.watch_view_one_Page);*/
+
+
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
+ /* GET update costume page */ 
+router.get('/update', secured,    
+Event_controlers.Event_update_Page); 
+
+/* GET create costume page */
+router.get('/create', secured, Event_controlers.Event_create_Page);
+
+/* GET delete costume page */
+router.get('/delete', secured, Event_controlers.Event_delete_Page);
+
+ 
 
 
 module.exports = router;
